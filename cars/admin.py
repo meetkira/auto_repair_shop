0be30b1a47fb1e096a9cfe1de4cdec179abbac55
Кристина Ратник <kristi.2001@yandex.ru>
2022-12-23 14:24:19 +0300

    add admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-'''from cars.models import Car
+from cars.models import Car
 
-admin.site.register(Car)'''
+admin.site.register(Car)
